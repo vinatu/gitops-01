868ba9f565d8411a2202e41c8a56e676631c05c4
vinatu <145034655+vinatu@users.noreply.github.com>
2024-08-27 23:38:42 +0100

    Update App.js
    ngrok webhook

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>what's the craic?</span>
+          <span style={{ color: 'red' }}>did it work</span>
         </div>
       </header>
     </div>

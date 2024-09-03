be29c9120f1cef8013a1119d418a35b3adb6be0e
vinatu <145034655+vinatu@users.noreply.github.com>
2024-09-03 12:04:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' /> 
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>final deploy test"</span>
+          <span style={{ color: 'red' }}>Deployed successfully"</span>
         </div>
       </header>
     </div>
